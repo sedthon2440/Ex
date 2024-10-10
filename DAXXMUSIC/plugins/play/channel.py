e9dc1aec4c4506a7492f98e0e492bddea0a3f6e6
sedthon2440 <166177796+sedthon2440@users.noreply.github.com>
2024-10-10 17:45:51 +0300

    Update channel.py

--- DAXXMUSIC/plugins/play/channel.py
+++ DAXXMUSIC/plugins/play/channel.py
@@ -2,14 +2,14 @@
 #▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯  T.me/ZThon   ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
 #▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯ T.me/Zelzal_Music ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
 
-from ZelzalMusic.plugins.play.filters import command
+from DAXXMUSIC.plugins.play.filters import command
 from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter, ChatMemberStatus, ChatType
 from pyrogram.types import Message
 
-from ZelzalMusic import app
-from ZelzalMusic.utils.database import set_cmode
-from ZelzalMusic.utils.decorators.admins import AdminActual
+from DAXXMUSIC import app
+from DAXXMUSIC.utils.database import set_cmode
+from DAXXMUSIC.utils.decorators.admins import AdminActual
 from config import BANNED_USERS
 
 
