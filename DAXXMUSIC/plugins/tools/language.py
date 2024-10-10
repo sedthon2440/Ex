253f5a623ade628b726f8bd1c41fdba1d19a23d0
sedthon2440 <166177796+sedthon2440@users.noreply.github.com>
2024-10-10 17:53:38 +0300

    Update language.py

--- DAXXMUSIC/plugins/tools/language.py
+++ DAXXMUSIC/plugins/tools/language.py
@@ -6,9 +6,9 @@ from pykeyboard import InlineKeyboard
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, Message
 
-from ZelzalMusic import app
-from ZelzalMusic.utils.database import get_lang, set_lang
-from ZelzalMusic.utils.decorators import ActualAdminCB, language, languageCB
+from DAXXMUSIC import app
+from DAXXMUSIC.utils.database import get_lang, set_lang
+from DAXXMUSIC.utils.decorators import ActualAdminCB, language, languageCB
 from config import BANNED_USERS
 from strings import get_string, languages_present
 
